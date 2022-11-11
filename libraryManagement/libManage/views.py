614bd948f20c1e596a7985582a867dcc3ec3a359
namdnhn <donhuhoangnam.work@gmail.com>
2022-11-11 17:28:21 +0700

    load img to template

--- libraryManagement/libManage/views.py
+++ libraryManagement/libManage/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    # response = HttpResponse()
+    # response.writelines('<h1>Hello</h1>')
+    # response.write('This is library management app')
+    # return response
+    return render(request, 'pages/base.html')
