9d26444039b2b50026b945ab1c0569b4eb670dc6
namdnhn <donhuhoangnam.work@gmail.com>
2022-11-16 00:17:55 +0700

    update

--- libraryManagement/libManage/views.py
+++ libraryManagement/libManage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,11 @@ def index(request):
     # response.write('This is library management app')
     # return response
     return render(request, 'pages/base.html')
+
+def list(request):
+    Data = {'Posts': Post.objects.all().order_by('-date')}
+    return render(request, 'blog/blog.html', Data)
+
+def post(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, 'blog/post.html', {'post': post})
